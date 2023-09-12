77475c68fa3237dd7839748924e2466a02767350
Đại Lộc Lê Quang <dailoc.lq@gmail.com>
2023-09-13 02:08:14 +0700

    init

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
+import {
+  PaperEmbeddedWalletSdk,
+  RecoveryShareManagement,
+} from "@paperxyz/embedded-wallet-service-sdk";
+
+function App() {
+  const [paperManaged, setPaperManaged] = useState();
+  useEffect(() => {
+    const paperManaged = new PaperEmbeddedWalletSdk({
+      clientId: "578407dc-6957-409e-832c-25b5a48a58ab",
+      chain: "Mumbai",
+      advancedOptions: {
+        recoveryShareManagement: RecoveryShareManagement.AWS_MANAGED,
+      },
+    });
+    setPaperManaged(paperManaged);
+  }, []);
+
+  const loginWithPaperModal = async () => {
+    try {
+      await paperManaged?.auth.loginWithPaperModal();
+    } catch (e) {
+      // use cancelled login flow
+    }
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <button
+          style={{ fontSize: "20px", padding: "6px 20px" }}
+          onClick={loginWithPaperModal}
+        >
+          Login WithPaper Modal
+        </button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
